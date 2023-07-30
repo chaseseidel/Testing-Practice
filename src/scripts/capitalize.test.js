98552d20d05d896a8d73788cd0efb46d309ee112
chaseseidel <chaseseidel16@gmail.com>
2023-07-30 12:10:42 -0700

    Create initial tests

---
+++ src/scripts/capitalize.test.js
@@ -0,0 +1,17 @@
+import capitalize from "./capitalize.js";
+
+test("Correctly capitalizes", () => {
+  expect(capitalize("hi")).toBe("Hi");
+});
+
+test("Check input value type", () => {
+  expect(() => capitalize(1)).toThrow();
+});
+
+test("Check string's first value", () => {
+  expect(() => capitalize("1")).toThrow();
+});
+
+test("Check string of length one", () => {
+  expect(capitalize("a")).toBe("A");
+});
